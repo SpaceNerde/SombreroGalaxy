d71d0222a0044997c0c7e6d1ef4ed4abfa4289b8
SpaceNerde <spacenerddev@gmail.com>
2025-08-20 18:16:09 +0200

    add field for downscaling of graph

--- sombrero-galaxy/main.py
+++ sombrero-galaxy/main.py
@@ -8,6 +8,8 @@ from PyQt6.QtWidgets import (
     QMainWindow,
     QLabel,
     QVBoxLayout,
+    QGridLayout,
+    QFormLayout,
     QPushButton,
     QFileDialog,
     QLineEdit,
@@ -26,17 +28,21 @@ from PyQt6.QtGui import (
 import pyqtgraph as pg
 
 class MainWindow(QMainWindow):
-    samplerate = 0
-    data = None 
+    ds = 1
 
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Sombrero Galaxy")
 
-        layout = QVBoxLayout()
         widget = QWidget()
-        
+
+        layout = QGridLayout()
+        audio_wave_layout = QVBoxLayout()
+        audio_wave_settings_layout = QFormLayout()
+
+        layout.addLayout(audio_wave_layout, 1, 1)
+
         self.audio_wave_graph = pg.PlotWidget()
         self.audio_wave_graph.setTitle("Waveform")
 
@@ -54,10 +60,22 @@ class MainWindow(QMainWindow):
         file_menu = menu.addMenu("File")
         file_menu.addAction(load_file_button)
 
-        layout.addWidget(self.audio_wave_graph)
+        self.downscale_field = QLineEdit(str(self.ds))
+        self.downscale_field.returnPressed.connect(self.downscale_change)
+
+        downscale_field_label = QLabel("downscale factor")
+
+        audio_wave_layout.addWidget(self.audio_wave_graph)
+        audio_wave_layout.addLayout(audio_wave_settings_layout)
+
+        audio_wave_settings_layout.addRow(downscale_field_label, self.downscale_field)
 
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+
+    def downscale_change(self):
+        self.ds = int(self.downscale_field.text())
+        self.update_plot()
 
     def open_file_dialog(self):
         file = QFileDialog.getOpenFileName(
@@ -73,15 +91,18 @@ class MainWindow(QMainWindow):
     
     def update_plot(self):
         time = np.linspace(0., self.data.shape[0] / self.samplerate, self.data.shape[0])
-        time = time[::10000]
+        time = time[::self.ds]
+
+        self.audio_wave_graph.clear()
+
         self.audio_wave_graph.plot(
             time, 
-            self.data[::10000, 0], 
+            self.data[::self.ds, 0], 
             pen=pg.mkPen('b', width=1.0)
         ).setSkipFiniteCheck(skipFiniteCheck=True)
         self.audio_wave_graph.plot(
             time,
-            self.data[::10000, 1], 
+            self.data[::self.ds, 1], 
             pen=pg.mkPen('g', width=1.0)
         ).setSkipFiniteCheck(skipFiniteCheck=True)
         
