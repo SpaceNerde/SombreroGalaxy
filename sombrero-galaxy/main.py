1a3e504b842f89e9bc2230da2ce1a17554a14e6e
SpaceNerde <spacenerddev@gmail.com>
2025-08-20 13:53:52 +0200

    create simple waveform vis using pyqtgraph

--- sombrero-galaxy/main.py
+++ sombrero-galaxy/main.py
@@ -1,5 +1,6 @@
 import sys
-import wave
+import numpy as np
+from scipy.io import wavfile
 
 from PyQt6.QtWidgets import (
     QApplication,
@@ -22,7 +23,12 @@ from PyQt6.QtGui import (
     QAction
 )
 
+import pyqtgraph as pg
+
 class MainWindow(QMainWindow):
+    samplerate = 0
+    data = None 
+
     def __init__(self):
         super().__init__()
 
@@ -30,6 +36,15 @@ class MainWindow(QMainWindow):
 
         layout = QVBoxLayout()
         widget = QWidget()
+        
+        self.audio_wave_graph = pg.PlotWidget()
+        self.audio_wave_graph.setTitle("Waveform")
+
+        ax_x = self.audio_wave_graph.getAxis("bottom")
+        ax_x.setLabel(text="time", units="s")
+
+        ax_y = self.audio_wave_graph.getAxis("left")
+        ax_y.setLabel(text="Amplitude")
 
         load_file_button = QAction("Load File", self)
         load_file_button.triggered.connect(self.open_file_dialog)
@@ -38,7 +53,9 @@ class MainWindow(QMainWindow):
 
         file_menu = menu.addMenu("File")
         file_menu.addAction(load_file_button)
-        
+
+        layout.addWidget(self.audio_wave_graph)
+
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
@@ -49,9 +66,26 @@ class MainWindow(QMainWindow):
             "./",
             ""
         )
+
+        self.samplerate, self.data = wavfile.read(file[0])
+
+        self.update_plot()
+    
+    def update_plot(self):
+        time = np.linspace(0., self.data.shape[0] / self.samplerate, self.data.shape[0])
+        time = time[::10000]
+        self.audio_wave_graph.plot(
+            time, 
+            self.data[::10000, 0], 
+            pen=pg.mkPen('b', width=1.0)
+        ).setSkipFiniteCheck(skipFiniteCheck=True)
+        self.audio_wave_graph.plot(
+            time,
+            self.data[::10000, 1], 
+            pen=pg.mkPen('g', width=1.0)
+        ).setSkipFiniteCheck(skipFiniteCheck=True)
         
-       #with wave.open(file[0]) as wav_file:
-       #     wav_file.close()
+        self.audio_wave_graph.setDownsampling(auto=True)
 
 def main():
     app = QApplication(sys.argv)
